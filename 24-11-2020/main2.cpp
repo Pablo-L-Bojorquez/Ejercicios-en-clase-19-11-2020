409566498802a652957c185a89da2e2ce29ef56e
Pablo-L-Bojorquez <73143735+Pablo-L-Bojorquez@users.noreply.github.com>
2020-11-24 21:49:40 -0700

    24/11/2020
    ejercicios en clase sobre las etiquetas.

---
+++ 24-11-2020/main2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{ // nivel 1
+	std::cout << "Ingrese un entero: ";
+	int value {};
+	std::cin >> value;
+	
+	if (value > 0)
+	{ // nivel 2
+		if ((value % 2) == 0)
+		{
+			std::cout << value << " es positivo y par \n";
+		}
+		else
+		{ // nivel 3
+			std::cout << value << " es positivo e impar \n";
+		}
+	}
+	
+	return 0;
+}
